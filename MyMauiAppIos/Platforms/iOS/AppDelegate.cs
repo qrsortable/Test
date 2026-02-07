482e3d5ee3204889d28f97cd5bb712bf2569d9b6
qrsortable <qrsortable@gmail.com>
2026-02-07 09:08:46 +0100

    Update AppDelegate.cs

--- MyMauiAppIos/Platforms/iOS/AppDelegate.cs
+++ MyMauiAppIos/Platforms/iOS/AppDelegate.cs
@@ -1,9 +1,30 @@
-﻿using Foundation;
+using Foundation;
+using UIKit;
+using Microsoft.Maui;
 
 namespace MyMauiAppIos;
 
 [Register("AppDelegate")]
 public class AppDelegate : MauiUIApplicationDelegate
 {
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    protected override MauiApp CreateMauiApp()
+        => MauiProgram.CreateMauiApp();
+
+    public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+    {
+        AppDomain.CurrentDomain.UnhandledException += (s, e) =>
+        {
+            Console.WriteLine("🔥 iOS UnhandledException:");
+            Console.WriteLine(e.ExceptionObject?.ToString());
+        };
+
+        TaskScheduler.UnobservedTaskException += (s, e) =>
+        {
+            Console.WriteLine("🔥 iOS UnobservedTaskException:");
+            Console.WriteLine(e.Exception?.ToString());
+            e.SetObserved();
+        };
+
+        return base.FinishedLaunching(app, options);
+    }
 }
